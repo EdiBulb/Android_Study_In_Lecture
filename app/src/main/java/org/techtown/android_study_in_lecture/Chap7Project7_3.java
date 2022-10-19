1dfdda4f54226d56bb5e1ca558deab84d1cd7e53
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-19 23:53:19 +0900

    Prepare for midterm

--- app/src/main/java/org/techtown/android_study_in_lecture/Chap7Project7_3.java
+++ app/src/main/java/org/techtown/android_study_in_lecture/Chap7Project7_3.java
@@ -1,14 +1,75 @@
 package org.techtown.android_study_in_lecture;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Chap7Project7_3 extends AppCompatActivity {
+
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chap7_project73);
+        setTitle("사용자 정보 입력");
+
+        tvName = findViewById(R.id.tvName);
+        tvEmail = findViewById(R.id.tvEmail);
+        button1 = findViewById(R.id.button1);
+
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogView = View.inflate(Chap7Project7_3.this,R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(Chap7Project7_3.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_launcher_background);
+                dlg.setView(dialogView);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dlgEdtName = dialogView.findViewById(R.id.dlgEdt1);
+                        dlgEdtEmail = dialogView.findViewById(R.id.dlgEdt2);
+
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                //대화상자 취소를 클릭했을 때
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = new Toast(Chap7Project7_3.this);
+                        toastView = View.inflate(Chap7Project7_3.this,
+                                R.layout.toast1, null);
+                        toastText = toastView.findViewById(R.id.toastText1);
+                        toastText.setText("취소했습니다.");
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+                dlg.show();
+
+
+            }
+
+
+        });
+
     }
 }
